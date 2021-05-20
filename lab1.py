9f9e510ca6e2f4d76b97874ef8cb8f20590b0b9b
kiararayecarganillo <kiaracarganillo@gmail.com>
2021-05-20 11:04:19 +0800

    adding pyhton pages

---
+++ lab1.py
@@ -0,0 +1,4 @@
+print("Name: Carganillo, Kiara Raye P.")
+print("Course and Section: SFIT2A")
+print("Contact Number: 09064316435")
+print("Email Address: kiara.raye.carganillo@gmail.com")
---
+++ script.py
@@ -0,0 +1,14 @@
+#print("Kiara Raye")
+#print("Carganillo")
+
+firstname = "Kiara Raye"
+lastname = "Carganillo"
+birthyear = 1999
+currentyear = 2021
+
+agenow = (currentyear - birthyear)
+#concatenation 
+print("Welcome " + firstname +" " + lastname +" " +str(agenow) +" years old")
+
+#place holder
+#print("Welcome %s %s %i " %(firstname,lastname,agenow))
